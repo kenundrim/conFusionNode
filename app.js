e248eab84c9fc5cc6117f1c04c92c37130fec22a
DJ Storms <dj.hughheffner@gmail.com>
2019-04-21 21:17:49 -0700

    Week 4 assignment

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-const uploadRouter = require('./routes/uploadRouter');
-var commentRouter = require('./routes/commentRouter');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var uploadRouter = require('./routes/uploadRouter');
+var favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -55,14 +55,14 @@ app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+app.use(auth);
+app.use('/dishes',dishRouter);
+app.use('/favorites',favoriteRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
-app.use('/comments',commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
